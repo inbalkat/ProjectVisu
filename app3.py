36c3730d6f9d9b7b6ee03543093b7b14698c6ada
inbalkat <inbalkat@post.bgu.ac.il>
2025-01-19 16:41:58 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -40,7 +40,7 @@ def plot_combined_salary_and_expenses(merged_df):
     width = 0.35  # the width of the bars
 
     # Define custom colors
-    salary_color = "dodgerblue"
+    salary_color = "deepskyblue"
     expenses_color = "lightcoral"
 
     ax.bar(x - width/2, merged_df["yearly_salary"], width, label="Yearly Salary", color=salary_color)
